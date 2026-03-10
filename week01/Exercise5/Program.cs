a517b4fa5f6d57e6e3c912124764a3f8c7bc9267
theophilox <njavier@byupathway.edu>
2026-03-10 22:05:13 +0900

    exercise5. remove all other function inside main function

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -15,42 +15,40 @@ class Program
 
         DisplayResult(name, sqt);
         
-
-        static void DisplayWelcome()
-        {
-            
-            Console.WriteLine("Welcome to the Program!");
-        }
-
-        static string PromptUserName ()
-        {
-            Console.WriteLine("Please enter your name: ");
-            string name = Console.ReadLine();
-            return name;
-        }
-
-        static int PromptUserNumber ()
-        {
-            Console.WriteLine("Please enter your favorite number: ");
-            string favorite = Console.ReadLine();
-            int number = int.Parse(favorite);
-            return number;
-
-        }
-
-         static int SquareNumber(int number)
-        {
-
-            int squared = number * number;
-            return squared;
-        }
-
-        static void DisplayResult(string name, int square)
-        {
-            Console.WriteLine($"{name}, the square of your number is {square}");
-        }
-
-
-
     }
+    static void DisplayWelcome()
+    {
+            
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName ()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber ()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string favorite = Console.ReadLine();
+        int number = int.Parse(favorite);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
+
+
+    
 }
