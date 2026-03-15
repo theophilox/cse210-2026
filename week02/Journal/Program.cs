beb3efcbcbda2247e76c3c179b9523a9caf8aeb7
theophilox <njavier@byupathway.edu>
2026-03-15 20:07:45 +0900

    Journal menu

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -3,7 +3,59 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+        
+        int menuAnswer = 0;
+
+        while (menuAnswer != 5)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Hello, What do you want to do?");
+            Console.WriteLine("\n---------------------------------------");
+            Console.WriteLine("1. Write new entry");
+            Console.WriteLine("2. Display my journal");
+            Console.WriteLine("3. Load my journal");
+            Console.WriteLine("4. Save my journal");
+            Console.WriteLine("5. Bye.");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("\nWhat would you like to do? ");
+
+            menuAnswer = int.Parse(Console.ReadLine());
+
+
+            if (menuAnswer == 1)
+            {
+
+            }
+            else if (menuAnswer == 2)
+            {
+                
+            }
+            else if (menuAnswer == 3)
+            {
+
+            }
+            else if (menuAnswer == 4)
+            {
+
+            }
+            else if (menuAnswer == 5)
+            {
+                Console.WriteLine("I'll be waiting to hear a great story from you again");
+                Console.WriteLine("Bye.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("----------Pick a valid action.---------");
+                Console.WriteLine("---------------------------------------");
+            }
+            
+        } 
+
     }
+    
 }
