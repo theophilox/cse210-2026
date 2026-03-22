cc0f70189954e6ba6974b5c0bd6f18fa44d784da
theophilox <njavier@byupathway.edu>
2026-03-22 20:42:33 +0900

    encapsulation

---
+++ week03/Fractions/fractions.cs
@@ -0,0 +1,55 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int SetTop(int top)
+    {
+        return _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public int SetBottom(int bottom)
+    {
+        return _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+    
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+
+
+}
