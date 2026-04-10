23660fe518008819756874dcef1225b681add47d
theophilox <njavier@byupathway.edu>
2026-04-10 18:18:46 +0900

    updated

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -7,10 +7,26 @@ class Program
         Console.WriteLine("Hello World! This is the Shapes Project.");
         
 
-        Square s = new Square("blue", 10);
-        
-        string color = s.GetColor();
-        double area = s.GetArea();
-        Console.WriteLine($"The area of the {color} square is {area}");
+        Square s = new Square("blue", 8);
+
+        //string color = s.GetColor();
+       // double area = s.GetArea();
+        //Console.WriteLine($"The area of the {color} square is {area}");
+
+        Rectangle r = new Rectangle("green", 27, 30);
+        Circle c = new Circle("pink", 8.82);
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The area of the {color} shape is {area}");
+
+        }
     }
 }
